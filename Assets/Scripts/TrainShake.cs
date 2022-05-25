0f59d2bf193ceb8a7d0cb2da7e58bdf7e735d619
Lairdom <kuraime@hotmail.com>
2022-05-25 15:56:32 +0300

    Train shake

--- Assets/Scripts/TrainShake.cs
+++ Assets/Scripts/TrainShake.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TrainShake : MonoBehaviour
 {
+    float timer, randomTime;
 
+    IEnumerator Shake() {
+        gameObject.transform.position = new Vector3(0,0.05f);
+        yield return new WaitForSeconds(0.2f);
+        gameObject.transform.position = new Vector3(0,0);
+    }
 
     void Start()
     {
-        
+        timer = 0;
     }
 
     void Update()
     {
-        
+        randomTime = Random.Range(8,15);
+        if (timer >= randomTime) {
+            StartCoroutine(Shake());
+            timer = 0;
+        }
+        timer += Time.deltaTime;
     }
 }
