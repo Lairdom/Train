bc895b3b2923e1481276b445133b6868ef8c59fe
myUserName <jovirta96@gmail.com>
2022-05-27 14:08:45 +0300

    Small changes to TrainShake.cs

--- Assets/Scripts/TrainShake.cs
+++ Assets/Scripts/TrainShake.cs
@@ -6,11 +6,15 @@ public class TrainShake : MonoBehaviour
 {
     float timer, randomTime;
 
+    [SerializeField] GameObject[] Carts;
+
     IEnumerator Shake() {
         if(BGManager.instance.IsMoving == true){
-            gameObject.transform.position = new Vector3(0,0.05f);
-            yield return new WaitForSeconds(0.2f);
-            gameObject.transform.position = new Vector3(0,0);
+            for (int i = 0; i<3; i++){
+                Carts[i].transform.position = new Vector3(Carts[i].transform.position.x,0.05f);
+                yield return new WaitForSeconds(0.2f);
+                Carts[i].transform.position = new Vector3(Carts[i].transform.position.x,0);
+            }
         }
     }
 
